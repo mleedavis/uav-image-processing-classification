0ebef792e6c792e6db257bb6bc982e985d10fdee
Lee Davis <Lee.Davis@ndow.org>
2022-10-13 12:42:32 -0700

    initial edits

---
+++ R/process-imagery.R
@@ -0,0 +1,48 @@
+#Create texture surfaces (entropy, variance, skewness) from raster image
+#bands and Canopy Height Model (CHM) from imported Digital Surface Model (DSM)
+#Digital Terrain Model (DTM)
+#
+#Note: import raster files as .tif
+
+#Load dependencies
+library(sf)
+library(raster)
+library(tmap)
+library(spatialEco)# for first-order occurrence measures on ortho image
+
+#Set WD
+setwd("") #<- change working directory here
+
+#Load Rasters
+
+#Load RGB bands here
+green.dat <- raster()
+  st_crs(green.dat) #<-obtain crs; WGS84/EPSG4326 should be standard
+
+#Load DSM and DTM
+dsm.dat <- raster("data/dsm.tif")
+dtm.dat <- raster("data/dtm/tif")
+
+#Calculate 1st order texture surfaces
+#Calculate neighborhood level entropy
+green.entropy <- raster.entropy(green.dat,
+                                d=15 #neighborhood 15 pixels
+  )
+  writeRaster(green.entropy, "output/OS_GreenEntropy.tif")
+
+#Calculate neighborhood level variance
+green.variance <- raster.moments(green.dat,
+                                   type="var",
+                                   s = 15) #neighborhood size 15 pixels
+
+writeRaster(green.entropy, "output/OS_GreenVariance.tif")
+
+#Calculate neighborhood level skewness
+green.skewness <- raster.moments(green.dat,
+                                   type="skew",
+                                   s = 15) #neighborhood size 15 pixels
+
+writeRaster(green.entropy, "output/OS_GreenSkewness.tif")
+
+#Calculate Canopy Height Model; subtract dtm from dsm on cell-by-cell basis
+chm <- dsm.dat-dtm.dat
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # uav-image-processing-classification
-Process and classify orthorectified UAV vegetation imagery to identify functional groups and species
+
+Example code flow to process and classify orthorectified UAV vegetation imagery 
+to identify functional groups and species
+
+This requires processing orthomosaics, Digital Terrain Models (DTM), and 
+Digital Surface Models (DSM) in 3rd party software
+
